0851254ed50a1d77c94f7593ec93a539418c9c0e
Anson Yu <ansonyu24@gmail.com>
2021-01-16 18:53:43 -0500

    fixed syntax

--- components/categories-cards.tsx
+++ components/categories-cards.tsx
@@ -2,38 +2,32 @@ import { Box, Text } from '@chakra-ui/react'
 import Head from 'next/head'
 import Nav from './nav'
 
-
 const categories = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      description: "Bret",
-      imageURL: "https://bit.ly/2Z4KKcF",
-    },
-    {
-        id: 2,
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz"
-    },
-    {
-      id: 3,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz"
-    },
-    {
-      id: 5,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz"
-    },
-
-
-}
-
-
-
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		description: 'Bret',
+		imageURL: 'https://bit.ly/2Z4KKcF',
+	},
+	{
+		id: 2,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz',
+	},
+	{
+		id: 3,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz',
+	},
+	{
+		id: 5,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz',
+	},
+]
 
 export default function OnboardingTitle() {
 	return (
